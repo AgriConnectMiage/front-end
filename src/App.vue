b22a4e7ec0b4302d7792ca470c93143090395b01
RemiSaurel <r.saurel5@gmail.com>
2024-03-16 00:26:22 +0100

    chore(shadcn): install dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
+import { Button } from "./components/ui/button";
+import { Input } from "@/components/ui/input";
 </script>
 
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <Button>Click me</Button>
+    <Input />
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
