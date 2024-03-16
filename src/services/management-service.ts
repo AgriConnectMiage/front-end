c28da2ddcac905bd110d7ff3cc10f1ebae2949f1
RemiSaurel <r.saurel5@gmail.com>
2024-03-17 00:16:53 +0100

    chore(api): add management const

--- src/services/management-service.ts
+++ src/services/management-service.ts
@@ -1,11 +1,11 @@
 import ky from "ky";
-import { API_URL } from "@/utils/constants.ts";
+import { API_MANAGEMENT, API_URL } from "@/utils/constants.ts";
 export interface Sensor {
   id: number;
 }
 
 const getAllSensors = async (): Promise<Sensor[]> => {
-  return await ky(API_URL + "/sensors").json();
+  return await ky(API_URL + API_MANAGEMENT + "/sensors").json();
 };
 
 export const managementService = {
