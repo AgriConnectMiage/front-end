15ef37a07643a4607331983dd02b56a1cb61fef6
RemiSaurel <r.saurel5@gmail.com>
2024-03-22 22:44:43 +0100

    feat(navbar): create component

--- src/components/navbar/VerticalNavbar.vue
+++ src/components/navbar/VerticalNavbar.vue
@@ -1,11 +1,44 @@
 <script setup lang="ts">
-
+import VerticalNavbarItem from "@/components/navbar/VerticalNavbarItem.vue";
 </script>
 
 <template>
-  $END$
+  <div class="hidden md:block">
+    <nav class="h-screen flex flex-col px-6 text-lg pt-8 bg-white" id="navbar">
+      <div
+        class="flex cursor-pointer pl-2 items-center"
+        @click="$router.push('/')"
+      >
+        <span class="ml-4 text-xl">ACM</span>
+      </div>
+
+      <!-- NAV BAR-->
+      <div class="flex flex-col items-start mt-8 gap-4">
+        <VerticalNavbarItem
+          label="Inventory"
+          routeName="Inventory"
+          :isActive="$route.name === 'Inventory'"
+        >
+        </VerticalNavbarItem>
+        <VerticalNavbarItem
+          label="Dashboard"
+          routeName="Dashboard"
+          :isActive="$route.name === 'Dashboard'"
+        >
+        </VerticalNavbarItem>
+        <VerticalNavbarItem
+          label="Profile"
+          routeName="Profile"
+          :isActive="$route.name === 'Profile'"
+        >
+        </VerticalNavbarItem>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <style scoped>
-
+#navbar {
+  width: 225px;
+}
 </style>
--- src/components/navbar/VerticalNavbarItem.vue
+++ src/components/navbar/VerticalNavbarItem.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
-
+defineProps({
+  label: String,
+  routeName: String,
+  isActive: Boolean,
+});
 </script>
 
 <template>
-  $END$
+  <div
+    class="flex py-2 pl-4 w-full rounded bg-transparent hover:bg-secondary-green cursor-pointer"
+    :class="{ 'bg-secondary-green': isActive }"
+    @click="$router.push({ name: routeName })"
+  >
+    <slot></slot>
+    <span class="text-neutral-500">{{ label }}</span>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
